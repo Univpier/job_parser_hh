2bf570d13454bd0c80ae3bb690738dc488abb4f5
Univpier <83532210+Univpier@users.noreply.github.com>
2023-02-21 16:27:41 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,98 +1,83 @@
-import requests
-from bs4 import BeautifulSoup
-import fake_useragent
-import time
 import json
-from datetime import datetime
-from itertools import count
-
-class Func():
-    def __init__(self):
-        self.c = 0
-    def __call__(self):
-        self.c += 1
-        return self.c  
-counter = Func()
-
-def get_links(text):
-    
-    ua = fake_useragent.UserAgent()
-    data = requests.get(
-        url=f"https://hh.ru/search/vacancy?text={text}&salary=&area=1&ored_clusters=true&enable_snippets=true&page=1",
-        headers={"user-agent":ua.random}
-    )
-    if data.status_code != 200:
-        return
-    soup = BeautifulSoup(data.content,"lxml")
-    try:
-        page_count = int(soup.find("div",attrs={"class":"pager"}).find_all("span",recursive=False)[-1].find("a").find("span").text)
-    except:
-        return
-    for page in range(page_count):
-        try:
-            data = requests.get(
-                url=f"https://hh.ru/search/vacancy?text={text}&salary=&area=1&ored_clusters=true&enable_snippets=true&page={page}",
-                headers={"user-agent":ua.random}
-            )
-            if data.status_code != 200:
-                continue
-            soup = BeautifulSoup(data.content, "lxml")
-            for a in soup.find_all("a",attrs={"class":"serp-item__title"}):
-                yield f"{a.attrs['href'].split('?')[0]}"
-        except Exception as e:
-            print(f"{e}")
-            time.sleep(1)
-def get_vacancy(link):
-    ua = fake_useragent.UserAgent()
-    data = requests.get(
-        url = link, 
-        headers={"user-agent":ua.random}
-    )
-    if data.status_code !=200:
-        return
-    soup = BeautifulSoup(data.content,"lxml")
-    try:
-        name = soup.find(attrs={"class":"vacancy-title"}).text.replace("з/п не указана","")
-    except:
-            name = ""
-    try:
-        url = link
-    except:
-            link = ""
-    try:
-        tags = [tag.text for tag in soup.find(attrs={"class":"bloko-tag-list"}).find_all(attrs={"class":"bloko-tag__section_text"})]
-    except:
-            tags = []
-    try:
-        time = str(datetime.now())
-    except:
-            time = ""
-    try:
-        count = counter()
-    except:
-            count = ""        
-
-    resume = {
-        "count":count,
-        "name":name,
-        "time":time,
-        "url":url,
-        "tags":tags
-     }
-    return resume
-
-if __name__ == "__main__":
-
-    data = []
-    for a in get_links("Специалист по технической защите информации"):
-        data.append(get_vacancy(a))
-        time.sleep(0.001)
-        with open("data.json", "w", encoding="utf-8") as f:
-            json.dump(data,f,indent=4,ensure_ascii = False)
-    
-
-    # for a in get_links("python"):
-    #     print(a)
-    # for a in get_links("python"):
-    #     print(get_resume(a))
-    #     time.sleep(1)
+from pprint import pprint
+import re
+from collections import Counter 
+with open('data2.json', 'r', encoding ='utf8') as f:
+    text = json.load(f)
+    txt = ""
+    pprint(text)
+    for group in text:
+        id_group = group['count']
+        for objects in group['tags']:
+         print(objects)
+with open('data2.json', 'r', encoding ='utf8') as f:
+    text = json.load(f)
+    txt = ""
+    tokens = []
+    print(text)
+    for group in text:
+        id_group = group['count']
+        for objects in group['tags']:
+            txt = txt + '\n' + objects
+            tokens.append(objects)         
+text = re.sub('["|(|)|«|»|,]', '' , txt)
+file_txt = open("text6.txt", "w", encoding="utf-8")
+file_txt.write(txt)
+file_txt.close()
+file_txt = open("tokens6.txt", "w", encoding="utf-8")
+for item in tokens:
+    file_txt.write("%s\n" % item)
+file_txt.close()
+print(tokens)
+print(text)   
+array_d = {}.fromkeys(tokens, 0)
+for a in tokens:
+    array_d[a] += 1
+if __name__ == '__main__':
+    c = Counter(array_d)
+    most = c.most_common()
+file_txt = open("popular6.txt", "w", encoding="utf-8")
+for item in most:
+    file_txt.write(f"{item}\n")
+file_txt.close()
+def pop_tokens(path):
+    with open(path, encoding="utf-8") as r:
+        all_words = re.findall(r"[0-9a-zA-Zа-яА-Я)]+", r.read())
+        txt = ""
+        for word in all_words:
+            clean2 = re.sub(r"[)]", ',' , word)
+            txt = clean2.split()
+            file_txt = open("text_tokens(Специалист по безопасности объектов критической инфраструктуры нефтегазового комплекса)6.txt", "a", encoding="utf-8")
+            for item in txt:
+                file_txt.write("%s " % item)
+            file_txt.close()        
+pop_tokens('popular6.txt')
+def count_words(path):
+    with open(path, encoding="utf-8") as file:
+        all_words = re.findall(r"[0-9a-zA-Zа-яА-Я]{3,}", file.read())
+        all_words = [word.upper() for word in all_words]
+        print('\nTotalWords:', len(all_words))
+        word_counts = Counter()
+        for word in all_words:
+            word_counts[word] +=1
+        print('\nTop Words:')
+        print(word_counts)
+        for word in word_counts.most_common(1000):
+            file_txt = open("text_popular.txt", "a", encoding="utf-8")
+            file_txt.write(word[0]+'\n')
+            file_txt.close()  
+def pop_words(path):
+    with open(path, encoding="utf-8") as r:
+        all_words = re.findall(r"[0-9a-zA-Zа-яА-Я]+", r.read())
+        for word in all_words:
+            with open('data2.json', 'r', encoding ='utf8') as r:
+                text = json.load(r)
+                txt = ""
+                for group in text:
+                    for objects in group['tags']:
+                        clean = re.sub('["|(|)|«|»|,]', '' , objects)
+                        txt = clean.split()
+                        for obj in txt:
+                            if word == obj:
+                                print(objects)
+pop_words('text_popular.txt')
